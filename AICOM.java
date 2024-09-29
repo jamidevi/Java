2489aefeda8b127616bcbc877bfaaab8ddf7fb68
jamidevi2081 <118243600+jamidevi@users.noreply.github.com>
2024-09-29 10:47:39 +0530

    29-09-2024_Java

---
+++ AICOM.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner read=new Scanner(System.in);
+		int x=read.nextInt();
+		if(x<=60){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
